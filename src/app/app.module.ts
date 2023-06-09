cf6ea901ce3a82b0ba5c4b4a7c7a5630721253c5
Javier <javibleu@hotmail.com>
2023-06-09 16:19:14 -0400

    feat: classes created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule } from "@angular/common/http";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { CourseComponent } from './courses/course/course.component';
+import { AboutComponent } from './about/about/about.component';
+import { CourseDialogComponent } from './courses/course-dialog/course-dialog.component';
+import { CoursesCardListComponent } from './courses/courses-card-list/courses-card-list.component';
+import { HomeComponent } from './courses/home/home.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    AboutComponent,
+    CourseDialogComponent,
+    CoursesCardListComponent,
+    HomeComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
