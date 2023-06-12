77635da058137165c9d74b6900d8c950feeb123f
Javier <javibleu@hotmail.com>
2023-06-12 17:28:55 -0400

    feat: about

--- src/app/about/about/about.component.spec.ts
+++ src/app/about/about/about.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AboutComponent ]
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(AboutComponent);
+      component = fixture.componentInstance;
+      el = fixture.debugElement;
 
-    fixture = TestBed.createComponent(AboutComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      fixture.detectChanges();
+    });
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show Welcome', () => {
+    const header = el.query(By.css('h1'));
+    expect(header.nativeElement.textContent).toContain('Welcome');
   });
 });
